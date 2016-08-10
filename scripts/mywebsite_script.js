ed6f74299e2229a6eca260099e37d38cab5be184
pjay79 <praveen.jayarajan@gmail.com>
2016-08-10 23:17:28 +1000

    Add velocity.js, remove jquery.ui, animated main h1 h2 p, add scroll svg

--- scripts/mywebsite_script.js
+++ scripts/mywebsite_script.js
@@ -19,6 +19,13 @@ var main = function() {
         $('.menu__overlay').toggleClass('menu__overlay--open');
     }
   });
+  // Animate site logo
+  $('.main h1').velocity("transition.slideLeftBigIn", { duration: 4500, delay : 1000 });
+  // Animate main h2 & p
+  $('.main h2').velocity("transition.slideUpBigIn", { duration: 4500, delay : 1000 });
+  $('.main p').velocity("transition.shrinkIn", { duration: 4500, delay : 1000 });
+  // Scroll arrow
+  $(".arrow").velocity({ translateY: "10px" },{ duration: 800, loop: true });
 };
 
 $(document).ready(main);
