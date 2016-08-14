81a43bef911d1dded5b832b89539b45ab67791df
pjay79 <praveen.jayarajan@gmail.com>
2016-08-14 23:03:02 +1000

    Add 1x and 2x landing page image, add social icons and animations

--- scripts/mywebsite_script.js
+++ scripts/mywebsite_script.js
@@ -1,7 +1,13 @@
 var main = function() {
-  // Set 100% viewport width/height to main background image
+  // Set 100% viewport width/height to div's
   var fullscreen = function(){
     $('.main').css({width: $(window).width(), height: $(window).height()});
+    $('.supporting').css({width: $(window).width(), height: $(window).height()});
+    $('.feature').css({width: $(window).width(), height: $(window).height()});
+    $('.feature2').css({width: $(window).width(), height: $(window).height()});
+    $('.footer').css({width: $(window).width(), height: $(window).height()});
+    $('.social').css({width: $(window).width(), height: $(window).height()/3});
+    $('.copy').css({width: $(window).width(), height: $(window).height()/5});
   };
   fullscreen();
   // Run the function again in case of window resize
@@ -9,23 +15,42 @@ var main = function() {
     fullscreen();
   });
   // Menu Overlay toggle
-  $('.menu').click(function(){
+  $('.main__menu').click(function(){
     $(this).toggleClass('active');
-    $('.menu__overlay').toggleClass('menu__overlay--open');
+    $('.main__menu--overlay').toggleClass('main__menu--overlay-open');
+    $('nav li').velocity("transition.slideDownIn", { stagger: 250, drag:true });
 	});
   // Menu Overlay toggle with 'esc' key
   $(document).keyup(function(e) {
     if (e.keyCode === 27) {
-        $('.menu__overlay').toggleClass('menu__overlay--open');
+        $('main__menu--overlay').toggleClass('main__menu--overlay-open');
     }
   });
-  // Animate site logo
-  $('.main h1').velocity("transition.slideLeftBigIn", { duration: 4500, delay : 1000 });
-  // Animate main h2 & p
-  $('.main h2').velocity("transition.slideUpBigIn", { duration: 4500, delay : 1000 });
-  $('.main p').velocity("transition.shrinkIn", { duration: 4500, delay : 1000 });
+  // Site logo
+  $('.header h1').velocity("transition.slideLeftBigIn", { duration: 800, delay : 400 });
+  // Main content
+  var loadingSequence = [
+    {e: $('.main__content') , p: { opacity: 1, translateY: 200 }, o: { duration : 800, delay : 400, easing: "spring" }}
+  ];
+  $.Velocity.RunSequence(loadingSequence);
   // Scroll arrow
-  $(".arrow").velocity({ translateY: "10px" },{ duration: 800, loop: true });
+  $('.main__arrow').velocity({ translateY: "10px" },{ duration: 800, loop: true });
+  $('.main__arrow').click(function() {
+    $('.supporting').velocity("scroll", 800);
+  });
+  // Fade div's in on scroll
+  $('.supporting').scroll(function() {
+    if ($(this).scrollTop() > 50) {
+        $('.supporting col-sm-12').velocity("fadeIn", { duration: 800, delay : 400 });
+    }
+  });
+  // Social icons
+  $('.social__icons img').mouseover(function(){
+    $(this).velocity( { scale : [ 1.4, [250, 25] ], rotateZ : "360deg" }, { duration : 400, easing: "ease-in-out" });
+    $(this).mouseleave(function(){
+      $(this).velocity( { scale : [ 1.0, [250, 25] ], rotateZ : "-360deg" }, { duration : 400, easing: "ease-in-out" });
+    });
+  });
 };
 
 $(document).ready(main);
