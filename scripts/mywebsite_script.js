456257e250dadec054339f191b08dc3620c25d21
pjay79 <praveen.jayarajan@gmail.com>
2016-08-22 15:45:16 +1000

    Refactor for semantic HTML, add scss color variables, scss font placeholders, and scrolldown svg

--- scripts/mywebsite_script.js
+++ scripts/mywebsite_script.js
@@ -1,67 +1,58 @@
 var main = function() {
   // Set 100% viewport width/height to div's
   var fullscreen = function(){
-    $('.main').css({width: $(window).width(), height: $(window).height()});
+    $('main').css({width: $(window).width(), height: $(window).height()});
+    $('section').css({width: $(window).width(), height: $(window).height()});
   };
   fullscreen();
   // Run the function again in case of window resize
   $(window).resize(function() {
     fullscreen();
   });
-  // Menu Overlay toggle
-  $('.main__menu').click(function() {
-    if (!$(this).hasClass('active')) {
-        $(this).addClass('active');
-        $('.main__menu--overlay').addClass('open');
-        $('nav li').velocity('stop')
-                   .velocity('transition.slideLeftBigIn', { duration: 600, stagger : 150, drag : true })
-                   .velocity({ scale : 1.3 }, { duration : 600, stagger : 150, drag : true, queue : false });
-    } else {
-        $(this).removeClass('active');
-        $('nav li').velocity('stop')
-                   .velocity({ scale : 0.5 }, { duration : 600, stagger : 150, drag : true, queue : false })
-                   .velocity('transition.slideRightBigOut', { duration : 600, stagger : 150, drag : true });
-        $('.main__menu--overlay').removeClass('open');
-    }
-  });
+  // Header
   var loadingSequence = [
     // Site logo
-    {e: $('.header h1') , p: 'transition.slideLeftBigIn', o: { duration : 400, delay : 300 }},
-    {e: $('.header h1') , p: {scale : 1.3}, o: { duration : 200 }},
-    {e: $('.header h1') , p: {scale : 1}, o: { duration : 200 }},
+    {e: $('main header h1') , p: 'transition.slideLeftBigIn', o: { duration : 400, delay : 300 }},
+    {e: $('main header h1') , p: {scale : 1.3}, o: { duration : 200 }},
+    {e: $('main header h1') , p: {scale : 1}, o: { duration : 200 }},
     // Main content
     {e: $('.main__content') , p: { opacity: 1, translateY: 200 }, o: { duration : 400, delay : 400, easing: 'ease-in-out', sequenceQueue : false }}
   ];
   $.Velocity.RunSequence(loadingSequence);
+  // Main menu overlay
+  $('.main__menu').click(function() {
+    if (!$(this).hasClass('active')) {
+      $(this).addClass('active');
+      $('.main__menu--overlay').addClass('open');
+      $('nav li').velocity('stop')
+      .velocity('transition.slideLeftBigIn', { duration : 300, easing : "ease" });
+    } else {
+      $(this).removeClass('active');
+      $('nav li').velocity('stop')
+      .velocity('transition.slideRightBigOut', { duration : 600, easing: "ease" });
+      $('.main__menu--overlay').removeClass('open');
+    }
+  });
   // Scroll arrow
   $('.main__arrow').velocity({ translateY: '10px' },{ duration: 800, loop: true });
   $('.main__arrow').click(function() {
-    $('.supporting').velocity('scroll', { duration : 800, offset : -75});
+    $('section').velocity('scroll', { duration : 800 });
   });
   // Sticky header
   $(window).scroll(function() {
     if ($(this).scrollTop() > 50) {
-      $('.header').addClass('sticky');
+      $('header').addClass('sticky');
     }
     else {
-      $('.header').removeClass('sticky');
+      $('header').removeClass('sticky');
     }
   });
   // ScrollMagic scenes
   var controller = new $.ScrollMagic.Controller();
-  var scene1 = new $.ScrollMagic.Scene({triggerElement: '#trigger_1'});
-  scene1.setVelocity('#animate_1', { opacity : 1 }, { duration : 1000, delay : 200, easing : 'ease-in-out' } );
+  var scene1 = new $.ScrollMagic.Scene({triggerElement: 'section'});
+  scene1.setVelocity('article', { opacity : 1 }, { duration : 1000, easing : 'ease-in-out' } );
   scene1.addTo(controller);
-  var scene2 = new $.ScrollMagic.Scene({triggerElement: '#trigger_2'});
-  scene2.setVelocity('#animate_2', { opacity : 1 }, { duration : 1000, delay : 200, easing : 'ease-in-out' } );
-  scene2.addTo(controller);
-  var scene3 = new $.ScrollMagic.Scene({triggerElement: '#trigger_3'});
-  scene3.setVelocity('#animate_3', { opacity : 1 }, { duration : 1000, delay : 200, easing : 'ease-in-out' } );
-  scene3.addTo(controller);
-  var scene4 = new $.ScrollMagic.Scene({triggerElement: '#trigger_4', offset: -100});
-  scene4.setVelocity('#animate_4', 'transition.bounceIn', { duration : 1200, delay : 400, easing : 'ease-in-out' } );
-  //scene4.addIndicators(); // add indicators (requires plugin)
-  scene4.addTo(controller);
+  //scene1.addIndicators(); // add indicators (requires plugin)
   // Social icons
   $('.social__icons img').mouseover(function(){
     $(this).velocity( { scale : 1.4, rotateZ : '360deg' }, { duration : 400, easing: 'ease-in-out' });
@@ -70,5 +61,4 @@ var main = function() {
     });
   });
 };
-
 $(document).ready(main);
