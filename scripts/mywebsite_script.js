eb89cc94f36060b75a919af5db828def27f8e437
pjay79 <praveen.jayarajan@gmail.com>
2016-11-28 18:30:12 +1100

    Add window scroll to top and force cache reload

--- scripts/mywebsite_script.js
+++ scripts/mywebsite_script.js
@@ -1,3 +1,8 @@
+  // Force scroll to top on refresh + force cache reload
+  $(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+  $(window).location.reload(true);
+  });
   // Set 100% viewport width/height to div's
   var fullscreen = function(){
     $('main').css({width: $(window).width(), height: $(window).height()});
@@ -14,7 +19,7 @@
   var main__animation = new TimelineMax({paused:true, delay: 0.5});
   main__animation.set($('.main__content'), {visibility: "visible"})
                  .from($('.main__content h2'), 0.5, {autoAlpha: 0, y: "-=100px", ease: Bounce.easeOut})
-                 .from($('.main__content h1'), 0.25, {autoAlpha: 0, ease: Power2.easeOut})
+                 .from($('.main__content h1'), 0.25, {autoAlpha: 0, x: "-=100px", ease: Power2.easeOut})
                  .from($('.main__content p:first-of-type'), 0.5, {autoAlpha: 0, scale: 1.2, ease: Back.easeOut})
                  .from($('.main__content p:last-child'), 0.5, {autoAlpha: 0, ease: Back.easeOut});
   main__animation.play();
@@ -61,33 +66,33 @@
   var controller = new $.ScrollMagic.Controller();
   var scene = new $.ScrollMagic.Scene({triggerElement: "#about"});
   var about__animation = new TimelineMax();
-  about__animation.from($('.about-me h3'), 1.5, {opacity: 0, width: 0, ease: Expo.easeOut}, 0.35)
-                  .from($('.about-skills h3'), 1.5, {opacity: 0, width: 0, ease: Expo.easeOut}, 0.35)
-                  .from($('.about-skills img'), 2.5, {opacity: 0, scale: 0, y: "+=100px", ease: Expo.easeOut}, 0)
-                  .staggerFrom($('.about-me p'), 1.5, {opacity: 0, y: "+=100px", ease: Expo.easeOut}, 0.35, 0)
-                  .staggerFrom($('.about-skills p'), 1.5, {opacity: 0, y: "+=100px", ease: Expo.easeOut}, 0.35, 0);
+  about__animation.from($('.about-me h3'), 1.5, {autoAlpha: 0, width: 0, ease: Expo.easeOut}, 0.35)
+                  .from($('.about-skills h3'), 1.5, {autoAlpha: 0, width: 0, ease: Expo.easeOut}, 0.35)
+                  .from($('.about-skills img'), 2.5, {autoAlpha: 0, scale: 0, y: "+=100px", ease: Expo.easeOut}, 0)
+                  .staggerFrom($('.about-me p'), 1.5, {autoAlpha: 0, y: "+=100px", ease: Expo.easeOut}, 0.35, 0)
+                  .staggerFrom($('.about-skills p'), 1.5, {autoAlpha: 0, y: "+=100px", ease: Expo.easeOut}, 0.35, 0);
 	scene.setTween(about__animation);
 	scene.addTo(controller);
   var scene1 = new $.ScrollMagic.Scene({triggerElement: '#portfolio'});
   var portfolio__animation = new TimelineMax();
-  portfolio__animation.from($('#portfolio h3'), 2.5, {opacity: 0, width: 0, ease: Expo.easeOut}, 0.35)
-                      .staggerFrom($('#portfolio figure'), 1.5, {opacity: 0, scale: 1.4, y: "+=100px", ease: Expo.easeOut}, 0.5, 0);
+  portfolio__animation.from($('#portfolio h3'), 2.5, {autoAlpha: 0, width: 0, ease: Expo.easeOut}, 0.35)
+                      .staggerFrom($('#portfolio figure'), 1.5, {autoAlpha: 0, scale: 1.4, y: "+=100px", ease: Expo.easeOut}, 0.5, 0);
   scene1.setTween(portfolio__animation);
   scene1.addTo(controller);
   var scene2 = new $.ScrollMagic.Scene({triggerElement: '#quote'});
   var quote__animation = new TimelineMax();
   quote__animation.from($('blockquote'), 1, {margin: "0, -5%", ease: Expo.easeOut})
-                  .staggerFrom($('#quote p:first-of-type'), 1, {opacity: 0, y: "+=100px", ease: Expo.easeOut}, 0.35, 0);
+                  .staggerFrom($('#quote p:first-of-type'), 1, {autoAlpha: 0, y: "+=100px", ease: Expo.easeOut}, 0.35, 0);
   scene2.setTween(quote__animation);
   scene2.addTo(controller);
   var scene3 = new $.ScrollMagic.Scene({triggerElement: '#contact'});
   var contact__animation = new TimelineMax();
-  contact__animation.from($('.contact__info h3'), 1.5, {opacity: 0, width: 0, ease: Expo.easeOut})
-                    .from($('.contact__info img'), 1.5, {opacity: 0, scale: 0, y: "+=200px", ease: Expo.easeOut}, 0)
-                    .from($('.contact__info a'), 1.5, {opacity: 0, y: "+=200px", ease: Expo.easeOut}, 0)
-                    .from($('.contact__info p'), 1.5, {opacity: 0, y: "+=200px", ease: Expo.easeOut}, 0)
-                    .from($('.contact__form h3'), 1.5, {opacity: 0, width: 0, ease: Expo.easeOut}, 0)
-                    .staggerFrom($('.contact__form div'), 1.5, {opacity: 0, y: "+=200px", ease: Expo.easeOut}, 0.25, 0);
+  contact__animation.from($('.contact__info h3'), 1.5, {autoAlpha: 0, width: 0, ease: Expo.easeOut})
+                    .from($('.contact__info img'), 1.5, {autoAlpha: 0, scale: 0, y: "+=200px", ease: Expo.easeOut}, 0)
+                    .from($('.contact__info a'), 1.5, {autoAlpha: 0, y: "+=200px", ease: Expo.easeOut}, 0)
+                    .from($('.contact__info p'), 1.5, {autoAlpha: 0, y: "+=200px", ease: Expo.easeOut}, 0)
+                    .from($('.contact__form h3'), 1.5, {autoAlpha: 0, width: 0, ease: Expo.easeOut}, 0)
+                    .staggerFrom($('.contact__form div'), 1.5, {autoAlpha: 0, y: "+=200px", ease: Expo.easeOut}, 0.25, 0);
   scene3.setTween(contact__animation);
   scene3.addTo(controller);
   //scene.addIndicators(); // add indicators (requires plugin)
@@ -113,7 +118,7 @@
   // Form submission
   $('form').submit(function() {
         var message__confirmation = new TimelineMax();
-            message__confirmation.to($('#confirmation'), 0.5, {opacity: 1, ease: Expo.easeOut})
+            message__confirmation.to($('#confirmation'), 0.5, {autoAlpha: 1, ease: Expo.easeOut})
                                  .to($('#confirmation'), 0.5, {autoAlpha: 0, ease: Expo.easeOut}, +0.5);
         event.preventDefault();
         var formData = $('form').serialize();
